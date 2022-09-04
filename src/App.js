1a12bcfb07805842cc4f8d39a3a5ca9d070bafd7
yanniro2 <k.niroyan@gmail.com>
2022-09-04 10:11:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./style.css"
+function App()
+{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="status-card">
+        <div className="status-card-container">
+          <div className="status-card-left">
+            <div className="status-card-left-text">
+              <h1 className="status-card-left-text-h1">
+                Get <span className="status-card-left-text-h1-span">insights</span> that help your business grow.
+              </h1>
+              <p className="status-card-left-text-p">
+                Discover the benefits of data analytics and make better decisions
+                regarding revenue, customer experience, and overall efficiency.
+              </p>
+            </div>
+            <div className="status-card-left-details">
+              <div className="status-card-left-details-companies">
+                <h1 className="status-card-left-details-companies-h1">10k+</h1>
+                <p className="status-card-left-details-companies-p">companies </p>
+              </div>
+              <div className="status-card-left-details-companies">
+                <h1 className="status-card-left-details-companies-h1">314 </h1>
+                <p className="status-card-left-details-companies-p">templates </p>
+              </div>
+              <div className="status-card-left-details-companies">
+                <h1 className="status-card-left-details-companies-h1">12M+ </h1>
+                <p className="status-card-left-details-companies-p">queries </p>
+              </div>
+            </div>
+          </div>
+          <div className="status-card-right">
+            <div className="status-card-right-image"></div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="attribution">
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge"
+        >Frontend Mentor</a
+        >. Coded by <a href="#">n12oyan</a>.
+      </div>
     </div>
   );
 }
